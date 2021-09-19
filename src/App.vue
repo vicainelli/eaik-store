626234e1a38bf8797e5285f424334861d0d19550
Vinicius Cainelli <vicainelli+github@gmail.com>
2021-09-19 21:27:27 +0200

    Add header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <div class="dark:bg-black dark:bg-opacity-50 py-4 dark:text-white">
+      <div class="mx-auto max-w-screen-xl">
+        <header class="flex justify-between">
+          <div>eaki store</div>
+          <nav class="">
+            <router-link class="mx-2" to="products">products</router-link>
+          </nav>
+          <div>cart</div>
+        </header>
+      </div>
+    </div>
     <router-view></router-view>
   </div>
 </template>
