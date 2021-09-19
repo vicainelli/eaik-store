c47a48e02f9feaaaf746fbdd232aa1ba2ce68b09
Vinicius Cainelli <vicainelli+github@gmail.com>
2021-09-19 21:27:43 +0200

    Add cart store module

---
+++ src/store/modules/cart.store.js
@@ -0,0 +1,32 @@
+const state = {
+  isLoading: false,
+  cart: [],
+}
+
+const getters = {
+  getCart: (state) => state.cart,
+  getIsLoading: (state) => state.isLoading,
+}
+
+const actions = {
+  addToCart({ commit }, product) {
+    commit('ADD_TO_CART', product)
+  },
+}
+
+const mutations = {
+  ADD_TO_CART: (state, payload) => {
+    const product = state.cart.find((item) => item.id === payload.id)
+    if (!product) state.cart.push({ ...payload, quantity: 1 })
+    else product.quantity = product.quantity + 1
+  },
+  SET_LOADING: (state, payload) => (state.isLoading = payload),
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations,
+}
